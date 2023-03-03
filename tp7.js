c37bdd32c011055a8b0d38c6b4e698643f8b6fe3
Corneille <cgbeaga@gmail.com>
2023-03-03 16:51:26 +0100

    TP 7 programmé

--- tp7.js
+++ tp7.js
@@ -1,1 +1,17 @@
+const nom = document.querySelector('#nom')
+const majuscule = document.querySelector('#majuscule');
 
+//Fonction pour toujours mettre en minuscule les données lors de la saisie
+nom.addEventListener('input', () => {
+    nom.value = nom.value.toLowerCase();
+})
+
+//Convertir en majucule à la sortie du champ
+/*majuscule.addEventListener('click', () => {
+    nom.value = nom.value.toUpperCase();
+})
+*/
+//Convertir en majucule à la sortie du champ
+nom.addEventListener('blur', () => {
+    nom.value = nom.value.toUpperCase();
+})
